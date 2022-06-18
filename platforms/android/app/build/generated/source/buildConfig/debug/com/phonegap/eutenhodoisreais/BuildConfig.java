feebd992c3185d2bc9da3157e3570ca6b0990293
Miqueias Caetano <miqueias.caetano@vistasoft.com.br>
2022-06-18 09:07:46 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/debug/com/phonegap/eutenhodoisreais/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/phonegap/eutenhodoisreais/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.phonegap.eutenhodoisreais";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10032;
-  public static final String VERSION_NAME = "1.0.32";
+  public static final int VERSION_CODE = 10037;
+  public static final String VERSION_NAME = "1.0.37";
 }
--- platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/eutenhodoisreais/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/eutenhodoisreais/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.phonegap.eutenhodoisreais";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 10034;
-  public static final String VERSION_NAME = "1.0.34";
+  public static final int VERSION_CODE = 10037;
+  public static final String VERSION_NAME = "1.0.37";
 }
